75f24730b71760f0f3b2fd18b9cfd4fef14a60cb
Eugene Cheltsov <e.cheltsov@corp.mail.ru>
2015-11-05 13:31:37 +0300

    wip

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 'use strict';
 
 const fs = require('fs');
-const PEG = require('pegjs');
+const Parser = require('./lib/parser.js').Parser;
+const festRender = require('./lib/renderer/fest.js');
 
-const festRender = require('./renderer/fest.js');
-const vdomRender = require('./renderer/vdom.js');
-
-const grammar = fs.readFileSync('grammars/fest.pegjs', 'utf8');
 const template = fs.readFileSync('templates/simple-with-params.xml', 'utf8');
+const parser = new Parser();
 
-const parser = PEG.buildParser(grammar);
 let ast = parser.parse(template);
-
-// let res = festRender(ast);
-// let res = vdomRender(ast);
-//
-
-fs.writeFile('ast/simple-with-params.json', JSON.stringify(ast, '', 2));
+let res = festRender(ast);
 
 console.log('fest -> AST:\n\n');
 console.log(JSON.stringify(ast, '', 2), '\n\n');
-// console.log('AST -> fest:\n\n');
+console.log('AST -> fest:\n\n');
+console.log(res);
