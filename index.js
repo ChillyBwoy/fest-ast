dab6cb1bcd5701a608f4b7130184ae88cd21b039
Eugene Cheltsov <e.cheltsov@corp.mail.ru>
2015-11-12 16:55:26 +0300

    Save parser to file

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const fs = require('fs');
-const Parser = require('./lib/parser.js').Parser;
-const festRender = require('./lib/renderer/fest.js');
+const render2Fest = require('./lib/renderer/fest.js');
+const parser = require('./lib/parser/fest.js');
 
 const template = fs.readFileSync('templates/simple-with-params.xml', 'utf8');
-const parser = new Parser();
 
 let ast = parser.parse(template);
-let res = festRender(ast);
+let res = render2Fest(ast);
 
 console.log('fest -> AST:\n\n');
 console.log(JSON.stringify(ast, '', 2), '\n\n');
